52a59ca853561c221273a79ebc8c53bfb10bd60b
Doug A <douganderson444@gmail.com>
2022-06-08 15:06:36 -0300

    reply with default props

--- src/lib/iframeSrc/InnerHandler.svelte
+++ src/lib/iframeSrc/InnerHandler.svelte
@@ -18,12 +18,17 @@
 	}
 
 	async function loadEsModuleComponent({ esModule, props }) {
+		// cleanup from prior loads
+		if (component) {
+			component.$destroy();
+			URL.revokeObjectURL(url); // memory management
+		}
+
 		const blob = new Blob([esModule], { type: 'text/javascript' });
 		url = URL.createObjectURL(blob);
 
 		import(url).then(function ({ default: App }) {
 			if (!App) return;
-			if (component) component.$destroy();
 
 			document.getElementById('app').innerHTML = '';
 			component = new App({
@@ -31,15 +36,23 @@
 				props: { ...props }
 			});
 
-			console.log({ ...component }); // accessors?
-			reply(...component); // trigger "rendered" in Gateway, plus push any default values to the db to save
+			// get the default values of component for each of the keys of component.$$.props
+			// this way the parent component can deduce the schema of the data
+			const initProps = component['$$'].props;
+			let inits = {};
+
+			Object.keys(initProps).map((k) => {
+				inits[k] = component[k]; // load default initial props
+			});
+
+			reply({ ...inits, ...props }); // trigger "rendered" in Gateway, plus push any default values to the db to save
 
 			// on change, Pass props message up to iframe parent so it can be saved/updated
 			component.$on('change', (event) => {
 				reply(event.detail);
 			});
 
-			URL.revokeObjectURL(url); // memory management
+			// URL.revokeObjectURL(url); // memory management
 		});
 	}
 
@@ -49,6 +62,7 @@
 	});
 </script>
 
+<!-- Svelte sets up a listener for us on the window object  -->
 <svelte:window on:message={handleMessage} />
 
 <!-- offsetHeight are readonly -->
