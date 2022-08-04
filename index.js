3a5219903fd524d20b3ff1a699361cf326ec71d1
Doug A <douganderson444@gmail.com>
2022-08-04 12:27:00 -0300

    refactor exports

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-export { default as default } from './src/lib/Gateway.svelte';
-export { default as Gateway } from './src/lib/Gateway.svelte';
-export { default as FetchComponent } from './src/lib/FetchComponent.svelte';
+export * from './src/lib/index.js';
