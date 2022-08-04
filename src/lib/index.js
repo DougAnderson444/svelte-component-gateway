3863f76977ca86fb99a2358fc13eb964166a581d
Doug A <douganderson444@gmail.com>
2022-08-04 12:24:08 -0300

    add default export

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,6 @@
-export { default as default } from './Gateway.svelte';
+// export { default as default } from './Gateway.svelte';
 export { default as Gateway } from './Gateway.svelte';
 export { default as FetchComponent } from './FetchComponent.svelte';
 
-// export CONSTANTS
 export { CHANGE as CHANGE } from './iframeSrc/constants.js';
+export { default as ComponentMounter } from './ComponentMounter.svelte';
